fb1b1725d624071c137139aff683413bc64a92c5
dooyou21 <cubeclock94@gmail.com>
2020-06-19 09:12:10 +0900

    BLD: typescript, webpack, tslint, prettier 설정

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
